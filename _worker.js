d1c3e6b9ae7739c6c3a2786d3508438756b747bc
CEOout <HengmeiliCEO@outlook.com>
2026-08-02 14:15:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -311,7 +311,7 @@ function generateHTML(备案内容) {
 	return `<!DOCTYPE html>
 <html lang="zh-CN">
 <head>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4955948896511841"
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9460979221320693"
      crossorigin="anonymous"></script>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
